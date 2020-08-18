f8e805457dfb3b6290e8c5b4aa928c48c1264961
hyomee <hyomee@naver.com>
2020-08-18 22:44:13 +0900

    속성외부화
    속성외부화

---
+++ EXP_PROPERTIES/src/main/java/co/kr/abacus/spring/properties/ExpPropertiesApplication.java
@@ -0,0 +1,13 @@
+package co.kr.abacus.spring.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpPropertiesApplication.class, args);
+	}
+
+}
---
+++ EXP_PROPERTIES/src/test/java/co/kr/abacus/spring/properties/ExpPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.abacus.spring.properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
