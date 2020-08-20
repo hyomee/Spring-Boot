c4e36148e3661bf582e154278b39d21cbb33e983
hyomee <hyomee@naver.com>
2020-08-20 22:31:03 +0900

    비동기 처리

---
+++ EXP_MVCASYNC/src/main/java/co/kr/abacus/spring/async/ExpMvcasyncApplication.java
@@ -0,0 +1,13 @@
+package co.kr.abacus.spring.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpMvcasyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpMvcasyncApplication.class, args);
+	}
+
+}
---
+++ EXP_MVCASYNC/src/test/java/co/kr/abacus/spring/async/ExpMvcasyncApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.abacus.spring.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpMvcasyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
