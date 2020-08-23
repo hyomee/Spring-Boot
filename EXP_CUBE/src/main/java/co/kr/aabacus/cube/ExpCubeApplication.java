05c2b2f25e75bb64e12ffb17f775338217e649f5
hyomee <hyomee@iabacus.co.kr>
2020-08-23 13:03:16 +0900

    .

---
+++ EXP_CUBE/src/main/java/co/kr/aabacus/cube/ExpCubeApplication.java
@@ -0,0 +1,13 @@
+package co.kr.aabacus.cube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpCubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpCubeApplication.class, args);
+	}
+
+}
---
+++ EXP_CUBE/src/test/java/co/kr/aabacus/cube/ExpCubeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.aabacus.cube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpCubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
