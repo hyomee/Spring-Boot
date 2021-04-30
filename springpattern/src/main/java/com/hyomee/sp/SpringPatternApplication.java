28d4fb0bb3676a63a3a58f7f39e61ece00645449
hyosang <hyomee@naver.com>
2021-05-01 02:13:31 +0900

    최초ㅓ밋

---
+++ springpattern/src/main/java/com/hyomee/sp/SpringPatternApplication.java
@@ -0,0 +1,13 @@
+package com.hyomee.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPatternApplication.class, args);
+	}
+
+}
---
+++ springpattern/src/test/java/com/hyomee/sp/SpringPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyomee.sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
